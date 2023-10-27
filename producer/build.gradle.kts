edffb737937d4c70d4edf93a5314603dc41c14f7
Robert Novotny <novotnr0@gmail.com>
2023-10-27 23:57:26 +0200

    Producer: Add support for RabbitMQ

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
