96d81cae9b0051c21b508d996731fcc2c8e57689
Robert Novotny <novotnr0@gmail.com>
2023-10-28 00:32:53 +0200

    Consumer: Consume a car from 'allCars' queue

--- consumer/src/main/kotlin/sk/upjs/ics/kopr/consumer/ConsumerApplication.kt
+++ consumer/src/main/kotlin/sk/upjs/ics/kopr/consumer/ConsumerApplication.kt
@@ -1,10 +1,21 @@
 package sk.upjs.ics.kopr.consumer
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import sk.upjs.ics.kopr.Car
 
 @SpringBootApplication
-class ConsumerApplication
+class ConsumerApplication {
+    private val logger: Logger = LoggerFactory.getLogger(ConsumerApplication::class.java)
+
+    @RabbitListener(queues = [ALL_CARS_QUEUE])
+    fun consumeAnyCar(car: Car) {
+        logger.info("Consuming car from '$ALL_CARS_QUEUE' queue: {}", car)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ConsumerApplication>(*args)
