8d75b9faaec3b4e3a772deb709baae643506653b
Robert Novotny <novotnr0@gmail.com>
2023-10-28 00:05:03 +0200

    Producer: Enable scheduling and periodically log a message

--- producer/src/main/kotlin/sk/upjs/ics/kopr/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/sk/upjs/ics/kopr/producer/ProducerApplication.kt
@@ -1,10 +1,22 @@
 package sk.upjs.ics.kopr.producer
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 
 @SpringBootApplication
-class ProducerApplication
+@EnableScheduling
+class ProducerApplication {
+    private val logger: Logger = LoggerFactory.getLogger(ProducerApplication::class.java)
+
+    @Scheduled(fixedDelay = 2000)
+    fun produceCar() {
+        logger.info("Producing a car")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ProducerApplication>(*args)
