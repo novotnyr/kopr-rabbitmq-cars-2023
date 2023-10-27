7ee9be9549fb69f5052f4164df759b696c3ce3cd
Robert Novotny <novotnr0@gmail.com>
2023-10-28 00:26:29 +0200

    Consumer: Add starters for RabbitMQ and JSON

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -21,6 +21,9 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-json")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
