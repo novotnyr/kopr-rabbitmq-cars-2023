9249f1d5c042abe38eac6182f3a5ec2444b7e9b0
Robert Novotny <novotnr0@gmail.com>
2023-10-28 00:11:29 +0200

    Producer: Publish a car message to 'car' exchange with proper routing key

--- producer/src/main/kotlin/sk/upjs/ics/kopr/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/sk/upjs/ics/kopr/producer/ProducerApplication.kt
@@ -2,6 +2,7 @@ package sk.upjs.ics.kopr.producer
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.amqp.core.AmqpTemplate
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -10,7 +11,7 @@ import sk.upjs.ics.kopr.CarRandomizer
 
 @SpringBootApplication
 @EnableScheduling
-class ProducerApplication {
+class ProducerApplication(val amqpTemplate: AmqpTemplate) {
     private val logger: Logger = LoggerFactory.getLogger(ProducerApplication::class.java)
 
     private val carRandomizer = CarRandomizer()
@@ -19,6 +20,7 @@ class ProducerApplication {
     fun produceCar() {
         val car = carRandomizer.randomCar()
         logger.info("Producing a car: {}", car)
+        amqpTemplate.convertAndSend(CAR_EXCHANGE, "car.${car.segment}", car)
     }
 }
 
