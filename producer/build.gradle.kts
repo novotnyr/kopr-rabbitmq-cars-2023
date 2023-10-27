5de89284d992bc325dec0b26c7980211acf4a8dd
Robert Novotny <novotnr0@gmail.com>
2023-10-28 00:07:11 +0200

    Producer: Add JSON dependencies

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-json")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
