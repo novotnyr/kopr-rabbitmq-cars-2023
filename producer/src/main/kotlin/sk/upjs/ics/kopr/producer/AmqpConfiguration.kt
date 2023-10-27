da4747c1d9d86bac189f308584c085ce24d38cba
Robert Novotny <novotnr0@gmail.com>
2023-10-28 00:10:13 +0200

    Producer: Declare 'car exchange'

--- producer/src/main/kotlin/sk/upjs/ics/kopr/producer/AmqpConfiguration.kt
+++ producer/src/main/kotlin/sk/upjs/ics/kopr/producer/AmqpConfiguration.kt
@@ -1,13 +1,20 @@
 package sk.upjs.ics.kopr.producer
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.amqp.core.ExchangeBuilder
+import org.springframework.amqp.core.TopicExchange
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
+const val CAR_EXCHANGE = "car"
 
 @Configuration
 class AmqpConfiguration {
     @Bean
     fun jsonMessageConverter(objectMapper: ObjectMapper)
         = Jackson2JsonMessageConverter(objectMapper)
+
+    @Bean
+    fun carExchange(): TopicExchange = ExchangeBuilder.topicExchange(CAR_EXCHANGE).build()
 }
