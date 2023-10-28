5209d8cce07348934ed8175dfffaaf277f37cd95
Robert Novotny <novotnr0@gmail.com>
2023-10-28 10:32:21 +0200

    Consumer: Demonstrate unreliable consumer

--- consumer/src/main/kotlin/sk/upjs/ics/kopr/consumer/ConsumerApplication.kt
+++ consumer/src/main/kotlin/sk/upjs/ics/kopr/consumer/ConsumerApplication.kt
@@ -2,6 +2,7 @@ package sk.upjs.ics.kopr.consumer
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.amqp.AmqpRejectAndDontRequeueException
 import org.springframework.amqp.core.ExchangeTypes.TOPIC
 import org.springframework.amqp.rabbit.annotation.Exchange
 import org.springframework.amqp.rabbit.annotation.Queue
@@ -10,6 +11,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import sk.upjs.ics.kopr.Car
+import kotlin.random.Random
 
 @SpringBootApplication
 class ConsumerApplication {
@@ -29,6 +31,16 @@ class ConsumerApplication {
     )
     fun consumeCompactCar(car: Car) {
         logger.info("Consuming compact car from '{}' queue: {}", COMPACT_CARS_QUEUE, car)
+    }
+
+    @RabbitListener(queues = [SMALL_CARS_QUEUE])
+    fun unreliableSmallCarConsumer(car: Car) {
+        if (car.make.contains("Kia")) {
+            throw AmqpRejectAndDontRequeueException("Will not process $car, message is dropped")
+        }
+        if (car.make.contains("Fiat") && Random.nextFloat() < 0.5) {
+            throw IllegalStateException("Will not process $car, message is returned to the queue")
+        }
     }
 }
 
